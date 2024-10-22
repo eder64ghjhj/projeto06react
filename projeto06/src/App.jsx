94d14216a022fe328ef95745827f5c7aa36e89fb
eder64ghjhj <166666766+eder64ghjhj@users.noreply.github.com>
2024-10-22 20:02:10 -0300

    Add files via upload

---
+++ projeto06/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import GaleriaDeImagens from './components/GaleriaDeImagens'
+import Imagem from './components/Imagem'
+import './App.css'
+
+function App() {
+  
+
+  return (
+     <>
+     <GaleriaDeImagens/>
+     <Imagem/>
+     </>
+  )
+}
+
+export default App
